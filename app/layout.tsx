b0a9a21e27a7cbbadbf4fbd746702e2a1b69fd1a
1aday <128118342+1aday@users.noreply.github.com>
2024-03-11 22:45:23 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,37 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
-
+import { Metadata } from 'next';
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  openGraph: {
+    title: 'Riggs9000 – Write Linkedin posts in your tone & style',
+    description: 'Riggs9000 is an AI bot built with your keeping your sanity in mind.',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://raw.githubusercontent.com/1aday/riggs9000/e74bfc13611960b31a4ad5f4d0d2054d20950ba2/public/RIGGS-9000.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://raw.githubusercontent.com/1aday/riggs9000/e74bfc13611960b31a4ad5f4d0d2054d20950ba2/public/RIGGS-9000.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+};
 
 export const metadata = {
   title: "Riggs9000 – Write Linkedin posts in your tone & style",
   description:
     "Riggs9000 is an AI bot built with your keeping your sanity in mind.",
-  image: "https://lh3.googleusercontent.com/d/1nhvMnsPOzclWtVOfPyB-2NeSnkW79zOu",
-  url: "https://riggs9000.vercel.app/",
-  og:image:"https://lh3.googleusercontent.com/d/1nhvMnsPOzclWtVOfPyB-2NeSnkW79zOu"
 }
 
 export default function RootLayout({
