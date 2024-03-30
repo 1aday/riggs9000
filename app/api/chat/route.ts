4f119265cb1a2c3f634051da729a9b286e12faf0
1aday <128118342+1aday@users.noreply.github.com>
2024-03-29 22:06:01 -0400

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,4 @@
-// ./app/api/chat/route.ts
-import OpenAI from 'openai'
+import { OpenAI } from 'openai'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 
 // Create an OpenAI API client (that's edge friendly!)
@@ -25,18 +24,17 @@ export async function POST(req: Request) {
         // used in the fine-tuning dataset
         content:
           'You are an AI assistant capable of writing informative content from an unbiased and analytical perspective. When responding to prompts, express views that emphasize clarity, data-driven insights, and objective analysis. Describe the stance towards opposing or aligned views by acknowledging different perspectives and citing relevant information where necessary. Approach to highlighting perceived issues or biases by presenting multiple angles and leaving personal opinions aside. Support your arguments with the latest statistics, credible studies, or authoritative reports that will resonate with a general audience interested in factual information. Maintain a level of impartiality in your perspective.'
-
-          },
+      },
       ...messages
     ],
-
-    temperature: 0.5, 
- frequency_penalty: 1.0,
-    max_tokens:4500
+    temperature: 0.5,
+    frequency_penalty: 1.0,
+    max_tokens: 4500
   })
 
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response)
+
   // Respond with the stream
   return new StreamingTextResponse(stream)
 }
